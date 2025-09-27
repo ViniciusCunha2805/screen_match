108dffe9e178b3dce5d92daf98997f8a613b947d
Vinícius Cunha <viniciussilvacunha2805@gmail.com>
2025-09-27 19:09:23 -0300

    subindo tudo

--- src/br/com/alura/principal/Principal.java
+++ src/br/com/alura/principal/Principal.java
@@ -1,17 +1,16 @@
+package br.com.alura.principal;
+
 import br.com.alura.screenmatch.calculadora.CalculadoraDeTempo;
 import br.com.alura.screenmatch.calculadora.FiltroRecomendacao;
 import br.com.alura.screenmtach.modelos.Episodio;
 import br.com.alura.screenmtach.modelos.Filme;
 import br.com.alura.screenmtach.modelos.Serie;
-import br.com.alura.screenmtach.modelos.Titulo;
 
-import java.awt.*;
+import java.util.ArrayList;
 
 public class Principal {
     public static void main(String[] args) {
-        Filme meuFilme =  new Filme();
-        meuFilme.setNome("Poderoso Chefão");
-        meuFilme.setAnoDeLancamento(1970);
+        Filme meuFilme =  new Filme("Poderoso Chefão", 1970);
         meuFilme.setDuracaoEmMinutos(180);
         System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
 
@@ -25,9 +24,7 @@ public class Principal {
 
         System.out.println(meuFilme.obterMedia());
 
-        Serie lost = new Serie();
-        lost.setNome("Lost");
-        lost.setAnoDeLancamento(2000);
+        Serie lost = new Serie("Lost", 2000);
         lost.setAtiva(true);
         lost.setTemporadas(15);
         lost.setEpPorTemp(100);
@@ -35,9 +32,7 @@ public class Principal {
         System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
         lost.exibeFichaTecnica();
 
-        Filme outroFilme =  new Filme();
-        outroFilme.setNome("Mr Pickles");
-        outroFilme.setAnoDeLancamento(2020);
+        Filme outroFilme =  new Filme("Mr Picles", 2020);
         outroFilme.setDuracaoEmMinutos(200);
 
         CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
@@ -54,5 +49,19 @@ public class Principal {
         ep.setSerie(lost);
         ep.setTotalVisualizacoes(300);
         filtro.filtra(ep);
+
+        var filmeDoPaulo = new Filme("DogVille", 2003);
+        filmeDoPaulo.setDuracaoEmMinutos(200);
+        filmeDoPaulo.avalia(10);
+
+        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(filmeDoPaulo);
+        listaDeFilmes.add(meuFilme);
+        listaDeFilmes.add(outroFilme);
+        System.out.println("Tamanho da lista " + listaDeFilmes.size());
+        System.out.println("Primeiro filme " + listaDeFilmes.get(0).getNome());
+        System.out.println(listaDeFilmes);
+        System.out.println(listaDeFilmes.get(0).toString());
+
     }
 }
--- src/br/com/alura/screenmtach/modelos/Titulo.java
+++ src/br/com/alura/screenmtach/modelos/Titulo.java
@@ -1,12 +1,17 @@
 package br.com.alura.screenmtach.modelos;
 
-public class Titulo {
+public class Titulo implements Comparable<Titulo>{
     private String nome;
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
     private int totalDeAvaliacoes;
     private int duracaoEmMinutos;
+
+    public Titulo(String nome, int anoDeLancamento) {
+        this.nome = nome;
+        this.anoDeLancamento = anoDeLancamento;
+    }
 
     public String getNome(){
         return nome;
@@ -50,5 +55,14 @@ public class Titulo {
 
     public int getDuracaoEmMinutos() {
         return duracaoEmMinutos;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    @Override
+    public int compareTo(Titulo outroTitulo) {
+        return this.getNome().compareTo(outroTitulo.getNome());
     }
 }
